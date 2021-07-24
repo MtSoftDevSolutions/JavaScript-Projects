a6da35958ea1be089388de464b687f52c892e934
ksig286 <66789857+ksig286@users.noreply.github.com>
2021-07-24 18:35:21 -0400

    Starting JS

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("Hello, World!"); //This alert box will pop up first
+
+var Fruit= "Tropical Fruits", long="banana", round="mango", coco="coconut"; //The variable fruit has been assigned tropical fruits, the variable 'long' has been assigned to banana, etc. 
+var long= long.fontcolor("yellow"); //I'm applying the color yellow to the variable long, which essentially means I'm making the word banana yellow.
+var round= round.fontcolor("red"); //Similar with banana I'm assigning the variable round to the color red which will make the output of the word mango red.
+var coco= coco.fontcolor("brown"); //similar to yellow and red.
+
+document.write(long, round, coco); //I told the program to print out all three fruits and their corresponding colors
+
+document.write(9+9); //Simple expression output will be 18.
