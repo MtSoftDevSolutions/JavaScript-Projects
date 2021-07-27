337bacad6a10cff4a46eb014c6705279cf573ac5
ksig286 <66789857+ksig286@users.noreply.github.com>
2021-07-27 14:07:12 -0400

    Fun with JS

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,35 @@
+var X=40;
+function Multiply_numbers_1() {
+    document.getElementById("mult1").innerHTML = (20*X + "<br>");
+}
+
+function addNumbers () {
+    var T = 300
+    document.getElementById("addy").innerHTML = (65 + T + "<br>");
+}
+function addNumbers2 () {
+    document.getElementbyId("addy2").innerHTML = (65 + N + "<br>");
+}
+
+function get_Date() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}
+
+
+function mercury() {
+    var temp = window.prompt ("How hot is it outside?");
+    let prompt;
+    if (temp > 90) {
+        prompt = "Good day for a pool day!";
+    }
+    else if (temp > 50 && temp <= 90) {
+        prompt= "The temp is just right.";
+    }
+    else {
+        prompt= "Dress warm.";
+    }
+    document.getElementById("temp").innerHTML = prompt;
+}
+    
